4d3b9351e21a2551cf2cd15bd99e242470fd829f
Rita Moraes <ritac.moraes@gmail.com>
2020-05-15 19:22:42 -0300

    [backend] - chapter 3 - until register user endpoint

--- models/User.js
+++ models/User.js
@@ -17,11 +17,11 @@ const UserSchema = new Schema({
   },
   avatar: {
     type: String,
-    required: true,
   },
   createAt: {
     type: Date,
     default: Date.now,
   },
 });
+
 module.exports = User = mongoose.model("users", UserSchema);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,41 @@
 const express = require("express");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+
 const router = express.Router();
 
+const User = require("../../models/User");
+
 router.get("/test", (req, res) => res.json({ msg: "Users works" }));
+
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", //Size
+        r: "pg", //Rating
+        d: "mm", //Default
+      });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password,
+      });
+      bcrypt.genSalt(10, (error, salt) => {
+        bcrypt.hash(newUser.password, salt, (error, hash) => {
+          if (error) throw error;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then((user) => res.json(user))
+            .catch(console.log(error));
+        });
+      });
+    }
+  });
+});
 
 module.exports = router;
