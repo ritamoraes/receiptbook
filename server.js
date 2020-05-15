023116f434987ea0c81d9b6cf02d736b8adb60c8
Rita Moraes <ritac.moraes@gmail.com>
2020-05-15 17:04:12 -0300

    [backend] - chapter 2 - until routes configuration

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
 const app = express();
 
 //Db config
@@ -16,3 +20,8 @@ app.get("/", (req, res) => res.send("Hellooo!"));
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server runing on port ${port}`));
+
+//Use routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
