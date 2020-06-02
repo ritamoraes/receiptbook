30e5b7bd9c833f6167629295f2c56b090c2a736d
Rita Moraes <ritac.moraes@gmail.com>
2020-06-02 18:59:33 -0300

    [backend] - chapter 3 - untion start to  validations

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,8 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+const validateRegisterInput = require("../../validation/register");
+
 const router = express.Router();
 
 const User = require("../../models/User");
@@ -12,9 +14,15 @@ const User = require("../../models/User");
 router.get("/test", (req, res) => res.json({ msg: "Users works" }));
 
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then((user) => {
     if (user) {
-      return res.status(400).json({ email: "Email already exists" });
+      errors.email = "Email already exists";
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", //Size
